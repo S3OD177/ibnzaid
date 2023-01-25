21c37b5eb74911f4b2e0cb51f91279c7df62836e
iMohmmedSA <81385827+iMohmmedSA@users.noreply.github.com>
2023-01-25 15:40:42 +0300

    Temp

--- EE/canvas.js
+++ EE/canvas.js
@@ -1,104 +1,46 @@
 var canvas = document.getElementById("canvas");
-var dwn = document.getElementById('btndownload')
-canvas.width= 1024;
-canvas.height= 1024;
-// canvas.width= window.innerWidth;
-// canvas.height= window.innerHeight;preview
+var ctx = canvas.getContext("2d");
+var img = new Image();
+var x = 0;
+var y = 0;
 
-document.getElementById("canvas").style.display = "none"; // after
-//document.getElementById("canvas").style.visibility = "hidden"; // before 
-document.getElementById("preview").style.display = "none"; // after
-//document.getElementById("preview").style.visibility = "hidden"; // before
+canvas.width= 1080;
+canvas.height= 1080;
 
-document.getElementById("download").style.display = "none"; //after
-//document.getElementById("download").style.visibility = "hidden"; // before
+function BackgorundEE() {
+    var fileInput = document.getElementById("BackgorundEE");
+    img.src = URL.createObjectURL(fileInput.files[0]);
 
-var context = canvas.getContext("2d");
-
-// var background = new Image();
-// background.src = "bg.png";
-// background.onload = function(){
-// context.drawImage(background,0,0);   
-// }
-
-function PreviewImage(input) {
-   
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        document.getElementById('preview').setAttribute("src",e.target.result);
+    img.onload = function() {            
+        // clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // draw the image on the canvas, scaling it to fit
+        ctx.drawImage(img, 0, 0, img.width, img.height, x, y, 1080, 1080);
     };
-    reader.readAsDataURL(input.files[0]);
-
-   
-    
 }
 
-function setImage() {
-    // document.getElementById('preview').setAttribute("src",'bg1.png');
-    
-    var image = new Image();
-    image.src = "bg1.png";
-    var image = document.getElementById('preview');
+function AddUserPhoto() {
+    var fileInput = document.getElementById("AddUserPhoto");
+    img.src = URL.createObjectURL(fileInput.files[0]);
 
-    // canvas.width= image.clientWidth;
-    // canvas.height= image.clientHeight;
-
-    context.drawImage(image,0,0,1024,1024);
-    // context.drawImage(image,50,50,500,500);
-
-    // context.drawImage(image, -190,0);
-    // context.drawImage(image, 0,-190,image.clientWidth*6,image.clientHeight*6);
-    // context.drawImage(image, canvas.width/4,400,400,300);
-
-    // WebFont.load({
-	// 	google: {
-	// 		families: ['Almarai:300,400,700']
-	// 	}
-	// });
-    context.font= '35px Almarai';
-    context.fillStyle= '#4A5456';
-    context.textAlign= 'center';
-
-    context.moveTo(0,canvas.height/2);
-  
-    context.fillText(document.getElementById("txtName").value,canvas.width/2,(canvas.height/2)+300);
-
-
-
-    document.getElementById("preview").style.visibility = "hidden";
-
-    var image = canvas.toDataURL("image/jpg");
-    // document.getElementById("download").href = image;
-    // document.getElementById("download").setAttribute("download", "myumage.jpg");
-    document.getElementById("download").setAttribute("href", image);
-    // setTimeout(function(){
-    //     //  alert("Hello"); 
-        
-        
-    //     }, 2000);
-    $('#download')[0].click();
-   
-    // document.getElementById("download").style.visibility = "visible";
-    // document.getElementById("download").disabled  = true;
-    // document.getElementById("canvas").style.visibility = "visible";
-    // setTimeout(function(){
-    //     //  alert("Hello"); 
-    // document.getElementById("download").disabled  = false;
-        
-    //     }, 6000);
-
-
-    
-    // $('#exampleModal').modal('show');
-
+    img.onload = function() {            
+        // clear the canvas
+        ctx.clearRect(0, 0, 540, 540);  // Where To clear {X} {Y} by {xd} {yd}
+        // draw the image on the canvas, scaling it to fit
+        ctx.drawImage(img, 0, 0, img.width, img.height, x, y, 540, 540);
+    };
 }
 
-
-download_img = function(el) {
- 
-  var image = canvas.toDataURL("image/jpg");
-  el.href = image;
-};
+function downloadEE() {
+    // get the data URL of the canvas
+    var dataURL = canvas.toDataURL("image/jpeg", 1.0);
+    // create an anchor element
+    var a = document.createElement("a");
+    a.download = "EE.jpg";
+    a.href = dataURL;
+    // simulate a click on the anchor element
+    a.click();
+  }
 
 
 
